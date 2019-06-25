29950735bf16515135250a6d6ec6ecf2895fbe8c
bruna <bs795365@gmail.com>
2019-06-25 16:55:19 -0300

    terminei de colocar os códigos na views e na página de perfil

--- core/views.py
+++ core/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+
 
 @login_required
 def perfil(request):
 	return render(request, 'perfil.html')
 
+def registro(request):
+	form = UserCreationForm(request.POST or None)
+	is form.is_valid():
+		form.save()
+		return redirect('login')
+	contexto = {
+		'form': form
+	}
+	return render(request, 'registro.html', contexto)
+
+@login_required
+def dados(request, id):
+	user = User.objects.get(pk=id)
+	form = UserCreationForm(request.POST or None, instance=user)
+	if form.is_valid():
+		form.save()
+		return redirect('perfil')
+	contexto = {
+		'form': form
+	}
+	return render(request, 'registro.html', contexto)
 # Create your views here.
