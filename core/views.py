c7ffabe66df895d2b367b80a886e3f80845bfccf
bruna <bs795365@gmail.com>
2019-06-25 15:45:33 -0300

    criei o template perfil e a pasta registration e dnetro dela está login. Elas vão servir na questão do login do usuário no site.

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def perfil(request):
+	return render(request, 'perfil.html')
 
 # Create your views here.
--- info419comida/urls.py
+++ info419comida/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from core.views import index, perfil, registro
 
 urlpatterns = [
+    #Parei na parte de registro do slide 9
+    path('registro/', registro, nama='registro'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('login/', auth_views.LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
 ]
