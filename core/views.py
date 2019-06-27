99dfcba694ac87820945e6947d58553a7d6df40b
bs795365@gmail.com <bs795365@gmail.com>
2019-06-27 11:24:52 -0300

    fiz alterações nas páginas index.html, perfil.html, login.html e urls.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 from .models import receita
 
 @login_required
@@ -25,7 +25,7 @@ def dados(request, id):
 		form.save()
 		return redirect('perfil')
 	contexto = {
-		'form': form
+	'form': form
 	}
 	return render(request, 'registro.html', contexto)
 
--- info419comida/urls.py
+++ info419comida/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from core.views import registro, dados, tipo, index
+from core.views import registro, dados, tipo, index, dados
 
 urlpatterns = [
     #Parei na parte de registro do slide 9
+    path('dados/<int:id>/', dados, name='dados'),
     path('', index, name='index'),    
     path('tipo/', tipo, name='tipo'),   
     path('dados/<int:id>/', dados, name='dados'),
