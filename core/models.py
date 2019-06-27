f8c3be43cd3f7e66c7b3ea3ccbd89460f1145f0e
Mirlayne <mirlayne.lopes@gmail.com>
2019-06-27 11:17:37 -0300

    ajeitei o negocio de imagem no models

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ class receita(models.Model):
 	titulo = models.CharField('Nome da receita:', max_length=100)
 	ingrediente = models.TextField('Ingredientes:')
 	preparo = models.TextField('Modo de preparo:')
+	imagem = models.ImageField('Imagem', upload_to='imagem', null=True)
 
 	
 # Create your models here.
--- info419comida/settings.py
+++ info419comida/settings.py
@@ -125,3 +125,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'perfil'
 
 LOGOUT_REDIRECT_URL = 'index'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
