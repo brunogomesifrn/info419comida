9d714cecf79ee66ba93816e60c9ede1f8d83466e
Mirlayne <mirlayne.lopes@gmail.com>
2019-06-28 21:00:13 -0300

    ajeitei pagina de perfil

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from .models import receita, Tipo
 from .forms import receitaForm, TipoForm
 
